d64aef17d7ad62d4ed7635acf86275c5a7e7acf4
ahoone <noahben-soussen@hotmail.fr>
2026-09-04 19:02:51 +0200

    flattened the venv command and corrected the common interface path

---
+++ cli/src/proxypi/commands/venv.py
@@ -0,0 +1,69 @@
+import subprocess
+from typing import Literal
+
+from proxypi.commands.dependencies.uv import uv
+from proxypi.common.config import PROJECT_ROOT
+
+
+def create_venv_from_requirements(
+    subdirectory: Literal["broker", "scraper", "tests"],
+):
+    _ = subprocess.run(
+        [
+            "uv",
+            "venv",
+            "--no-project",
+            "--clear",
+            "--python",
+            "3.10",
+            f"{PROJECT_ROOT}/{subdirectory}/.venv",
+        ],
+        check=True,
+    )
+
+    _ = subprocess.run(
+        [
+            "uv",
+            "pip",
+            "install",
+            "--link-mode",
+            "copy",
+            "--python",
+            f"{PROJECT_ROOT}/{subdirectory}/.venv/bin/python",
+            "-r",
+            f"{PROJECT_ROOT}/{subdirectory}/requirements.txt",
+            "-e",
+            f"{PROJECT_ROOT}/common",
+        ],
+        check=True,
+    )
+
+
+def create_venv_from_toml(subdirectory: Literal["common", "cli"]):
+
+    _ = subprocess.run(
+        [
+            "uv",
+            "sync",
+            "--link-mode",
+            "copy",
+            "--directory",
+            f"{PROJECT_ROOT}/{subdirectory}",
+        ],
+        check=True,
+    )
+
+
+def venv():
+    """
+    Creates virtual environments for each component (broker, scraper, common, CLI) for development.
+    Intended to be launch on the IDE's host while the project is on a remote SSH server.
+    """
+    if not uv.is_satisfied:
+        raise ImportError("uv is not verifying conditions")
+
+    create_venv_from_requirements("broker")
+    create_venv_from_requirements("scraper")
+    create_venv_from_requirements("tests")
+    create_venv_from_toml("common")
+    create_venv_from_toml("cli")
--- cli/src/proxypi/main.py
+++ cli/src/proxypi/main.py
@@ -1,9 +1,9 @@
 from typer import Typer
 
 import proxypi.commands.deploy
-import proxypi.commands.dev
 import proxypi.commands.init
 import proxypi.commands.ssh
+from proxypi.commands.venv import venv
 
 app = Typer()
 
@@ -11,7 +11,8 @@ app = Typer()
 app.add_typer(proxypi.commands.ssh.app, name="ssh")
 app.add_typer(proxypi.commands.deploy.app, name="deploy")
 app.add_typer(proxypi.commands.init.app, name="init")
-app.add_typer(proxypi.commands.dev.app, name="dev")
+app.command()(venv)
+
 
 if __name__ == "__main__":
     app()
